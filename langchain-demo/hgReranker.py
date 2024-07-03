c9edaf00c351c910ab18ff42532fbb0d73314116
caozhiyong <caozhiyong@yanxue.com>
2024-07-03 16:00:10 +0800

    add langchain rerank demo

---
+++ langchain-demo/hgReranker.py
@@ -0,0 +1,48 @@
+import os
+
+os.environ['KMP_DUPLICATE_LIB_OK'] = 'TRUE'
+
+# Helper function for printing docs
+
+
+def pretty_print_docs(docs):
+    print(
+        f"\n{'-' * 100}\n".join(
+            [f"Document {i+1}:\n\n" + d.page_content for i, d in enumerate(docs)]
+        )
+    )
+
+from langchain_community.document_loaders import TextLoader
+from langchain_community.embeddings import JinaEmbeddings
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+documents = TextLoader(
+    "./state_of_the_union.txt","UTF-8"
+).load()
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
+texts = text_splitter.split_documents(documents)
+
+embedding = JinaEmbeddings(model_name="jina-embeddings-v2-base-en")
+retriever = FAISS.from_documents(texts, embedding).as_retriever(search_kwargs={"k": 20})
+
+query = "What did the president say about Ketanji Brown Jackson"
+docs = retriever.get_relevant_documents(query)
+
+from langchain.retrievers import ContextualCompressionRetriever
+from langchain.retrievers.document_compressors import CrossEncoderReranker
+from langchain_community.cross_encoders import HuggingFaceCrossEncoder
+import time
+
+t1 = time.time_ns() / 1_000_000
+model = HuggingFaceCrossEncoder(model_name="BAAI/bge-reranker-base")
+compressor = CrossEncoderReranker(model=model, top_n=3)
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=compressor, base_retriever=retriever
+)
+
+compressed_docs = compression_retriever.invoke("What is the plan for the economy?")
+pretty_print_docs(compressed_docs)
+
+t2 = time.time_ns() / 1_000_000
+print("used", t2-t1, "ms")
