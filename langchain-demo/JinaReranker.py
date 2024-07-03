1c96ec9a67baece6152e0daaeb8face22474a0cc
caozhiyong <caozhiyong@yanxue.com>
2024-07-03 16:35:19 +0800

    use WebBaseLoader

--- langchain-demo/JinaReranker.py
+++ langchain-demo/JinaReranker.py
@@ -12,22 +12,19 @@ def pretty_print_docs(docs):
         )
     )
 
-from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import WebBaseLoader
 from langchain_community.embeddings import JinaEmbeddings
 from langchain_community.vectorstores import FAISS
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-documents = TextLoader(
-    "./state_of_the_union.txt","UTF-8"
+documents = WebBaseLoader(
+    "https://jina.ai/reranker/"
 ).load()
 text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=100)
 texts = text_splitter.split_documents(documents)
 
 embedding = JinaEmbeddings(model_name="jina-embeddings-v2-base-en")
 retriever = FAISS.from_documents(texts, embedding).as_retriever(search_kwargs={"k": 20})
-
-query = "What did the president say about Ketanji Brown Jackson"
-docs = retriever.get_relevant_documents(query)
 
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import CrossEncoderReranker
@@ -45,7 +42,7 @@ compression_retriever = ContextualCompressionRetriever(
 )
 
 compressed_docs = compression_retriever.get_relevant_documents(
-    "What did the president say about Ketanji Jackson Brown"
+    "what is reranker?"
 )
 pretty_print_docs(compressed_docs)
 t2 = time.time_ns() / 1_000_000
